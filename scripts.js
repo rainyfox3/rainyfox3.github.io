aa4d07128180a40d5f5d4734859cb0b39762c998
rainyfox3 <135923790+rainyfox3@users.noreply.github.com>
2024-12-24 06:37:00 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-onst images = document.querySelectorAll('.o1, .o2, .o3, .o4, .o5, .o6, .o7, .o8, .o9, .o10, .o11, .o12, .o13, .o14, .o15, .o16, .o17, .o18, .o19, .o20, .o21, .o22, .o23, .o24');
+const images = document.querySelectorAll('.o1, .o2, .o3, .o4, .o5, .o6, .o7, .o8, .o9, .o10, .o11, .o12, .o13, .o14, .o15, .o16, .o17, .o18, .o19, .o20, .o21, .o22, .o23, .o24');
 
 const defaultOpacity = '0'; // Set the default opacity
 let o24State = parseInt(localStorage.getItem('o24State')) || 0; // Retrieve o24State from local storage
